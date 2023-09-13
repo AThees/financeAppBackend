17866a445a78715e30d095114ce27dcc1b0f0997
AThees <alvaro@thees.com.br>
2023-09-12 21:19:28 -0300

    Fist commit

---
+++ src/main/java/com/example/financeappbackend/FinanceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.financeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financeappbackend/FinanceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financeappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

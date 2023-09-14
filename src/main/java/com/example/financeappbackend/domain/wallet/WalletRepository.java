ba34ce74cd77efe55fa101635674bf26cdec4422
Álvaro Thees <alvaro@thees.com.br>
2023-09-13 21:31:53 -0300

    Create WalletRepository

---
+++ src/main/java/com/example/financeappbackend/domain/wallet/WalletRepository.java
@@ -0,0 +1,5 @@
+package com.example.financeappbackend.domain.wallet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletRepository extends JpaRepository<Wallet, Integer> {}

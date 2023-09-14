fd39a052ddfadd8503a380ceb28ab80b366beb0f
Álvaro Thees <alvaro@thees.com.br>
2023-09-13 22:44:45 -0300

    Alter Repository and entity id data type Int -> String to use UUID

--- src/main/java/com/example/financeappbackend/domain/wallet/Wallet.java
+++ src/main/java/com/example/financeappbackend/domain/wallet/Wallet.java
@@ -1,25 +1,32 @@
 package com.example.financeappbackend.domain.wallet;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.financeappbackend.dtos.WalletDTO;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Entity(name = "wallets")
+@Entity(name="wallets")
+@Table(name="wallets")
+@ToString
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Wallet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
 
-    String name;
-    Integer amountInCents;
-    String financeInstitution;
-    String type;
-    Boolean addToSum;
+    private String name;
+    private Integer amount_in_cents;
+    private String finance_institution;
+    private String type;
+    private Boolean add_to_sum;
+
+    public Wallet(WalletDTO data){
+        this.name = data.name();
+        this.amount_in_cents = data.amountInCents();
+        this.finance_institution = data.financeInstitution();
+        this.type = data.type();
+        this.add_to_sum = data.addToSum();
+    }
 }
--- src/main/java/com/example/financeappbackend/domain/wallet/WalletRepository.java
+++ src/main/java/com/example/financeappbackend/domain/wallet/WalletRepository.java
@@ -2,4 +2,4 @@ package com.example.financeappbackend.domain.wallet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, Integer> {}
+public interface WalletRepository extends JpaRepository<Wallet, String> {}

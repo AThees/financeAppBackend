386aeb5bdd650bc2c0fef88738f9276b1b935799
Álvaro Thees <alvaro@thees.com.br>
2023-09-15 20:35:22 -0300

    Created Expense class

---
+++ src/main/java/com/example/financeappbackend/domain/expense/Expense.java
@@ -0,0 +1,9 @@
+package com.example.financeappbackend.domain.expense;
+
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Table
+public class Expense {
+}

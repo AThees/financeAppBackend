60b7b0230d5bdfe529f809e9718892ee99403bf5
Álvaro Thees <alvaro@thees.com.br>
2023-09-17 02:11:01 -0300

    Update: configured Expense entity following the database modeling

--- src/main/java/com/example/financeappbackend/domain/expense/Expense.java
+++ src/main/java/com/example/financeappbackend/domain/expense/Expense.java
@@ -1,9 +1,37 @@
 package com.example.financeappbackend.domain.expense;
 
-import jakarta.persistence.Table;
-import org.springframework.stereotype.Repository;
+import com.example.financeappbackend.domain.wallet.Wallet;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Repository
-@Table
+
+@Entity
+@Table(name="expenses")
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Expense {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String description;
+    private Long value_in_cents;
+    private Boolean paid;
+    private String category;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet walletId;
+
+    public Expense(ExpenseDTO data, Wallet wallet){
+        this.description = data.description();
+        this.category = data.category();
+        this.value_in_cents = data.valueInCents();
+        this.paid = data.paid();
+        this.walletId = wallet;
+    }
 }

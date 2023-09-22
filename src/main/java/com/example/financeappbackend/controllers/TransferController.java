75b40bb3456c25d5ee232a7f9f767f70422c3adf
Álvaro Thees <alvaro@thees.com.br>
2023-09-22 16:49:16 -0300

    feat: create new transfer between 02 wallets

--- src/main/java/com/example/financeappbackend/controllers/TransferController.java
+++ src/main/java/com/example/financeappbackend/controllers/TransferController.java
@@ -1,15 +1,26 @@
 package com.example.financeappbackend.controllers;
 
+import com.example.financeappbackend.domain.transfer.TransferDTORequest;
+import com.example.financeappbackend.domain.transfer.TransferDTOResponse;
 import com.example.financeappbackend.domain.transfer.TransferService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transfer")
 public class TransferController {
 
-    TransferService transactionService;
+    final
+    TransferService transferService;
 
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
 
+    @PostMapping()
+    public ResponseEntity<TransferDTOResponse> createTransfer(@RequestBody TransferDTORequest transfer){
+        TransferDTOResponse answer = transferService.createTransfer(transfer);
+        return ResponseEntity.ok(answer);
+    }
 
 }
--- src/main/java/com/example/financeappbackend/domain/transfer/TransferService.java
+++ src/main/java/com/example/financeappbackend/domain/transfer/TransferService.java
@@ -1,7 +1,58 @@
 package com.example.financeappbackend.domain.transfer;
 
+import com.example.financeappbackend.domain.wallet.Wallet;
+import com.example.financeappbackend.domain.wallet.WalletRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransferService {
+
+    final
+    TransferRepository transferRepository;
+
+    final
+    WalletRepository walletRepository;
+
+    public TransferService(WalletRepository walletRepository, TransferRepository transferRepository) {
+        this.walletRepository = walletRepository;
+        this.transferRepository = transferRepository;
+    }
+
+    public TransferDTOResponse createTransfer(TransferDTORequest transfer){
+
+        Optional<Wallet> senderWallet = walletRepository.findById(transfer.senderWallet());
+        Optional<Wallet> receiverWallet = walletRepository.findById(transfer.receiverWallet());
+
+        if(senderWallet.isEmpty() || receiverWallet.isEmpty()){
+            return null;
+        }
+
+        Wallet senderWalletObj = senderWallet.get();
+        Wallet receiverWalletObj = receiverWallet.get();
+
+        if(senderWalletObj.getAmount_in_cents() >= transfer.valueInCents()){
+            senderWalletObj.setAmount_in_cents(senderWalletObj.getAmount_in_cents() - transfer.valueInCents());
+            receiverWalletObj.setAmount_in_cents(receiverWalletObj.getAmount_in_cents() + transfer.valueInCents());
+            walletRepository.save(senderWalletObj);
+            walletRepository.save(receiverWalletObj);
+        }
+
+        Transfer newTransfer = new Transfer(
+                transfer.description(),
+                transfer.valueInCents(),
+                senderWalletObj,
+                receiverWalletObj);
+
+        transferRepository.save(newTransfer);
+
+        return new TransferDTOResponse(
+                newTransfer.getSender_wallet_id(),
+                newTransfer.getReceiver_wallet_id(),
+                newTransfer.value_in_cents,
+                newTransfer.getDescription(),
+                newTransfer.getId()
+        );
+    }
 }

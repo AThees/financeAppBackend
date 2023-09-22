1e8d0c246ec37a98032ea4e572fff3e029ea528e
AThees <alvaro@thees.com.br>
2023-09-22 14:28:36 -0300

    Created transaction entity

---
+++ src/main/java/com/example/financeappbackend/domain/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package com.example.financeappbackend.domain.transaction;
+
+import com.example.financeappbackend.domain.wallet.Wallet;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="transactions")
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String description;
+    Long value_in_cents;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "sender_wallet_id", nullable = false)
+    private Wallet sender_wallet_id;
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "receiver_wallet_id", nullable = false)
+    private Wallet receiver_wallet_id;
+
+
+}

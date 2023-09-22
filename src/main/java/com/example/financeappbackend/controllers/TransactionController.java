3f3326f23cc59518a44134e9391facf6eb70fefb
AThees <alvaro@thees.com.br>
2023-09-22 14:29:29 -0300

    Created transaction controller

---
+++ src/main/java/com/example/financeappbackend/controllers/TransactionController.java
@@ -0,0 +1,9 @@
+package com.example.financeappbackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+}
